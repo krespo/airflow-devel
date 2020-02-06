9a4bd728a8fd878c31cb689ecc3e6651010a7614
louis <louis.jeon@kakaocorp.com>
2020-02-06 14:55:10 +0900

    setting

--- sbin/setup.sh
+++ sbin/setup.sh
@@ -4,7 +4,7 @@
 command -v brew > /dev/null 2>&1 || { echo >&2 "Homebrew가 설치되지 않았습니다. Homebrew를 먼저 설치해 주세요."; exit 1;  }
 command -v direnv > /dev/null 2>&1 || { echo >&2 "direnv가 설치되지 않았습니다. direnv를 먼저 설치해 주세요."; exit 1;  }
 
-AIRFLOW_HOME="$(cd -P "$(dirname "${filename}")/..";pwd)"
+AIRFLOW_HOME=$( cd "$(dirname "$0")/../" ; pwd )
 
 
 ## create .envrc
@@ -13,10 +13,12 @@ printf 'layout python3\nexport AIRFLOW_HOME='$AIRFLOW_HOME > $AIRFLOW_HOME/.envr
 direnv allow
 
 ## airflow installation
-pip install -r ${AIRFLOW_HOME}/requirements.txt
+echo '[Install] pip install Airflow'
+pip install -r ${AIRFLOW_HOME}/requirements.txt > /dev/null
+echo '[Success] pip install Airflow'
 
 ## airflow initialize
-airflow initdb
+airflow initdb > /dev/null
 
 echo -e "\033[0;33m##############[[[[ 설정 완료 ]]] #########################\033[0m"
-echo "airflow.cfg를 수정하고 실행하세요"
+echo "\033[0;33mairflow.cfg를 수정하고 실행하세요\033[0m"
